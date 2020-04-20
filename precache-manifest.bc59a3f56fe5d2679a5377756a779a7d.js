287646c4882d509d2872a68c8435905edb2bcd7c
Dmitriy Bondarenko <demianbondarenko@gmail.com>
2020-04-20 20:56:31 +0200

    Updates

---
+++ precache-manifest.bc59a3f56fe5d2679a5377756a779a7d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09b9d3b3ac2a696e0e661327733879a9",
+    "url": "/react-burger/index.html"
+  },
+  {
+    "revision": "8acb38ca09151b061c5d",
+    "url": "/react-burger/static/css/main.e234ed16.chunk.css"
+  },
+  {
+    "revision": "44d02af8065c4b5e4140",
+    "url": "/react-burger/static/js/2.3be3ac4f.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/react-burger/static/js/2.3be3ac4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8acb38ca09151b061c5d",
+    "url": "/react-burger/static/js/main.02d587a8.chunk.js"
+  },
+  {
+    "revision": "f7cf52c6088ef6fb346a",
+    "url": "/react-burger/static/js/runtime-main.776c47b7.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/react-burger/static/media/logo.b8503d26.png"
+  }
+]);
